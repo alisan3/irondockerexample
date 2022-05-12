f7be2ce8b6bdf30ea881426cd60efd29679cffdf
Alexander Bohle <alexander.bohle@gmail.com>
2022-05-12 16:01:28 +0100

    wip

--- Example/Program.cs
+++ Example/Program.cs
@@ -5,6 +5,16 @@ IronPdf.Logging.Logger.LogFilePath = "Default.log"; //May be set to a directory 
 IronPdf.Logging.Logger.LoggingMode = IronPdf.Logging.Logger.LoggingModes.All;
 
 var Renderer = new ChromePdfRenderer();
-using var PDF = Renderer.RenderHtmlAsPdf("<h1>Html with CSS and Images</h1>");
+Renderer.RenderingOptions.HtmlFooter = new HtmlHeaderFooter()
+{
+    MaxHeight = 15, //millimeters
+    HtmlFragment = "<center><i>footer<i></center>",
+    DrawDividerLine = true
+};
+
+using var PDF = Renderer.RenderHtmlAsPdf("<h1>Html with CSS and Images</h1>")
+    .WatermarkAllPages("<h1>test</h1>");
 
 PDF.SaveAs("pixel-perfect.pdf");
+
+Thread.Sleep(TimeSpan.FromMinutes(3));
